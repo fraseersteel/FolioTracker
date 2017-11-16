c4e77741304a9c16fd09b68e45379c3418b9d18b
fraser steel <frasersteel@btinternet.com>
2017-11-16 15:44:10 +0000

    GUI and controller stuff

--- src/controller/PortfolioListener.java
+++ src/controller/PortfolioListener.java
@@ -3,6 +3,7 @@ package controller;
 import model.IPortfolio;
 import model.IPortfolioTracker;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -21,13 +22,25 @@ public class PortfolioListener implements ActionListener {
 //        String selectedTickerName = stockTable.getSelectedTicker();
 //        int numOfShares           = stockTable.getSelectedNum();
         switch(action){
-            case "buy":
-                //            folioModel.buyStock(selectedTickerName, numOfShares);
+            case "Open":
+                String newName;
+                newName = JOptionPane.showInputDialog("New Name");
+                System.out.println(newName);
                 break;
-            case "save":
-
+            case "Save":
+                portfolio.savePortfolios();
                 break;
-            case "delete":
+            case "Sell All":
+                int n = JOptionPane.showConfirmDialog(null,"Are you sure you would like to sell all?","Sell all",JOptionPane.YES_NO_OPTION);
+                System.out.println(n);
+                if(n==0){
+                //sell all stocks
+                }else{
+                // do nothing
+                }
+                break;
+            case "Exit":
+                System.exit(0);
                 break;
         }
     }
