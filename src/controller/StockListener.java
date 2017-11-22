88cf20d5b2c78cc075ed3ef18fb89d7ecb332c66
xbb15134 <kuen.chan.2015@uni.strath.ac.uk>
2017-11-22 20:53:42 +0000

    loading message pop up, does not currently display components

--- src/controller/StockListener.java
+++ src/controller/StockListener.java
@@ -32,54 +32,73 @@ public class StockListener implements ActionListener, TableModelListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-            String action = e.getActionCommand();
-            String ticker = stockTable.getSelectedTicker();
+        String action = e.getActionCommand();
+        String ticker = stockTable.getSelectedTicker();
 
-            if(ticker == null && !action.equals(ADD)){
-                displayError("Row not selected\nPlease select the row with the desired stock.");
-                return;
+        if (ticker == null && !action.equals(ADD)) {
+            displayError("Row not selected\nPlease select the row with the desired stock.");
+            return;
+        }
+        boolean inputAccepted = false;
+        while (!inputAccepted) {
+
+            int result = JOptionPane.showConfirmDialog(null, getPanel(action.equals(ADD)),
+                    action, JOptionPane.OK_CANCEL_OPTION);
+
+            if (result == JOptionPane.OK_OPTION) {
+                inputAccepted = doAction(action, ticker);
+            } else {
+                inputAccepted = true;
             }
-            boolean inputAccepted = false;
-            while(!inputAccepted) {
-
-                int result = JOptionPane.showConfirmDialog(null, getPanel(action.equals(ADD)),
-                        action, JOptionPane.OK_CANCEL_OPTION);
-
-                if (result == JOptionPane.OK_OPTION) {
-                    inputAccepted = doAction(action, ticker);
-                } else {
-                    inputAccepted = true;
-                }
-            }
+        }
     }
 
-    private boolean doAction(String action, String ticker){
-        try{
+    private boolean doAction(String action, String ticker) {
+        try {
             int i = Integer.parseInt(sharesAmountField.getText());
-            if(i > 1000000000 || i < 1){
+            if (i > 1000000000 || i < 1) {
                 displayError("Please enter a number within the range 1 to 1000000000.");
-            }else{
+            } else {
                 Boolean pass;
                 //TODO start loading animation
-                if(action.equals(ADD)){
-                    ticker = tickerField.getText().replaceAll("\\s+","");
+                JDialog dialog = new JDialog();
+                dialog.setTitle("Waiting");
+
+                JPanel pane = new JPanel();
+                dialog.setContentPane(pane);
+
+                pane.add(new JLabel("WAITING"));
+
+                dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+                dialog.pack();
+                dialog.setVisible(true);
+                dialog.setAlwaysOnTop (true);
+                System.out.println("successful");
+
+
+                if (action.equals(ADD)) {
+                    ticker = tickerField.getText().replaceAll("\\s+", "");
                     pass = folioModel.buyStock(ticker, i);
-                }else if(action.equals(BUY)){
+                } else if (action.equals(BUY)) {
                     pass = folioModel.buyStock(ticker, i);
-                }else{
+                } else {
                     assert action.equals(SELL) : action;
                     pass = folioModel.sellStock(ticker, i);
                 }
+
+
+                dialog.dispose();
+
                 //TODO stop loading animation
-                if(pass == null){
+                if (pass == null) {
                     displayError("Not enough shares\n you have tried to sell more shares than you own!");
-                }else if(pass){
+                } else if (pass) {
                     return true;
-                }else{
+                } else {
                     displayError("No such Ticker Found. [" + ticker + "]. The ticker doesn't exist.");
                 }
             }
-        }catch (NumberFormatException err){
+        } catch (NumberFormatException err) {
             displayError("Please enter a number for the number of shares.");
         }
         return false;
@@ -89,12 +108,12 @@ public class StockListener implements ActionListener, TableModelListener {
         JPanel myPanel = new JPanel();
         GridLayout gridLayout;
 
-        if(withTickerField){
-            gridLayout = new GridLayout(2,2);
+        if (withTickerField) {
+            gridLayout = new GridLayout(2, 2);
             myPanel.add(new JLabel("Ticker:"));
             myPanel.add(tickerField);
-        }else{
-            gridLayout = new GridLayout(1,2);
+        } else {
+            gridLayout = new GridLayout(1, 2);
         }
         myPanel.setLayout(gridLayout);
 
@@ -104,7 +123,7 @@ public class StockListener implements ActionListener, TableModelListener {
         return myPanel;
     }
 
-    private void displayError(String msg){
+    private void displayError(String msg) {
         JOptionPane.showMessageDialog(
                 null,
                 msg,
@@ -115,7 +134,7 @@ public class StockListener implements ActionListener, TableModelListener {
     @Override
     public void tableChanged(TableModelEvent e) {
         if (e.getType() == TableModelEvent.UPDATE) {
-            if(e.getColumn() == 1){
+            if (e.getColumn() == 1) {
                 String ticker = stockTable.getSelectedTicker();
                 IStock stock = folioModel.getStockByTicker(ticker);
                 stock.setStockName((String) stockTable.getValueAt(e.getFirstRow(), e.getColumn()));
